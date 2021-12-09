01234ce581eab58e2857ce4fd182b3a8204f1e45
Jose Castillo <jcastillo@hypernovalabs.com>
2021-12-08 21:46:02 -0500

    Crear el layout de inicio y el menu

--- ProyectoFinal_Castillo_Lan_Morn/app/src/main/java/com/example/proyectofinal_castillo_lan_morn/MainActivity.java
+++ ProyectoFinal_Castillo_Lan_Morn/app/src/main/java/com/example/proyectofinal_castillo_lan_morn/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.proyectofinal_castillo_lan_morn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnInciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInciar = findViewById(R.id.btnIniciar);
+
+        btnInciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+            }
+        });
     }
 }
